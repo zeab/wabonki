9783bba3deaa9ecce5203f1bda52a98b9282a8b6
kevin downs <pyrosrppltoo@gmail.com>
2018-12-21 23:28:03 -0800

    0.0.4

--- src/main/scala/zeab/logging/Logging.scala
+++ src/main/scala/zeab/logging/Logging.scala
@@ -2,26 +2,36 @@ package zeab.logging
 
 //Imports
 import zeab.envgrok.EnvGrok
-//Logback
-import ch.qos.logback.classic.{Logger => Logback}
+//Other
 import ch.qos.logback.classic.{Level, LoggerContext}
-//Slf4j
 import org.slf4j.{Logger, LoggerFactory}
 
-trait Logging extends EnvGrok {
+trait Logging extends EnvGrok{
 
-  val log: Logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
-
+  //Grab the logging context
   val loggerContext: LoggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
 
-  val rootLogger: Logback = loggerContext.getLogger(Logger.ROOT_LOGGER_NAME)
-  val rootLogLevel: Level = envGrok("ROOT_LOG_LEVEL", "INFO").toUpperCase match {
-    case "ERROR" => Level.ERROR
-    case "WARN" | "WARNING" => Level.WARN
-    case "INFO" => Level.INFO
-    case "DEBUG" => Level.DEBUG
-    case "OFF" => Level.OFF
+  //Set all the log levels
+  val rootLogLevel: Level = getLogLevel("ROOT_LOG_LEVEL", "INFO")
+  loggerContext.getLogger(Logger.ROOT_LOGGER_NAME).setLevel(rootLogLevel)
+  val akkaLogLevel: Level = getLogLevel("AKKA_LOG_LEVEL", "INFO")
+  loggerContext.getLogger("file").setLevel(akkaLogLevel)
+
+  //Log to console
+  val log: Logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
+
+  //Log to file
+  val fileLog: Logger = LoggerFactory.getLogger("file")
+
+  def getLogLevel(key:String, default:String): Level ={
+    envGrok(key, default).toUpperCase match {
+      case "ERROR" => Level.ERROR
+      case "WARN" | "WARNING" => Level.WARN
+      case "INFO" => Level.INFO
+      case "DEBUG" => Level.DEBUG
+      case "OFF" => Level.OFF
+      case "TRACE" => Level.TRACE
+    }
   }
-  rootLogger.setLevel(rootLogLevel)
 
 }
