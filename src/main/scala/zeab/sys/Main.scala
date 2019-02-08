b2579e057ff4422a674412ddb63ba7eabb940a61
kevin downs <pyrosrppltoo@gmail.com>
2019-02-07 21:32:06 -0800

    more env prop stuff

--- src/main/scala/zeab/sys/Main.scala
+++ src/main/scala/zeab/sys/Main.scala
@@ -1,8 +1,14 @@
 package zeab.sys
 
+import scala.util.Random
+
 object Main extends EnvVars {
 
   def main(args: Array[String]): Unit = {
+
+
+    val aa = List("a", "b")
+    val hh = Random.shuffle(aa).take(1)
 
 
     val gg = getEnvVarAsString("home", "llama")
