0123ade8a8d40e21aa01c79d5fbdd75297cb44dd
kevin downs <pyrosrppltoo@gmail.com>
2019-04-02 13:41:00 -0700

    moving in stuff from before

--- src/main/scala/scalatestreboot/scalatestexamples/MyFirstTest.scala
+++ src/main/scala/scalatestreboot/scalatestexamples/MyFirstTest.scala
@@ -1,5 +1,21 @@
 package scalatestreboot.scalatestexamples
 
-class MyFirstTest {
+//Imports
+import org.scalatest.{FunSuite, ParallelTestExecution}
+
+//with ParallelTestExecution
+class MyFirstTest extends FunSuite {
+
+  test("1 + 1 == 2"){
+    assert(1 + 1 == 2)
+  }
+
+  test("0 + 0 == 0"){
+    assert(0 + 0 == 0)
+  }
+
+  test("2 + 2 == 4"){
+    assert(2 + 2 == 1)
+  }
 
 }
--- src/main/scala/scalatestreboot/scalatestexamples/Run.scala
+++ src/main/scala/scalatestreboot/scalatestexamples/Run.scala
@@ -4,8 +4,15 @@ object Run {
 
   def main(args: Array[String]): Unit = {
 
+    (new MyFirstTest).execute(testName = "1 + 1 == 2")
 
+    //The lastest ones
+    //val specName: String = "scalatestreboot.scalatestexamples.MyFirstTest"
+    //val x = org.scalatest.tools.Runner.run(Array("-o", "-s", specName, "-P16"))
 
+    //The ones from back in alasc days...
+
+    //val x = org.scalatest.tools.Runner.run(Array("-R", ".", "-oF", "-P8", "-s", specName))
 
   }
 
